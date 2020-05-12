a35ce0453d28e9314a8d1fb752bdc9cbc424acb4
yfz34 <allen27029@gmail.com>
2020-05-12 18:02:09 +0800

    Updates

---
+++ precache-manifest.6bb83d52a3323fffe0ec202fdd144a57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68700fd4931ef410138a3fc6c5db9679",
+    "url": "/myweb/index.html"
+  },
+  {
+    "revision": "b1065e78ee3954390366",
+    "url": "/myweb/static/css/main.57c4a7f4.chunk.css"
+  },
+  {
+    "revision": "749e2d6c14d55c744e6e",
+    "url": "/myweb/static/js/2.c7d6117f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/myweb/static/js/2.c7d6117f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1065e78ee3954390366",
+    "url": "/myweb/static/js/main.1c1321e5.chunk.js"
+  },
+  {
+    "revision": "035333a7861e1a639ee1",
+    "url": "/myweb/static/js/runtime-main.a28de002.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
